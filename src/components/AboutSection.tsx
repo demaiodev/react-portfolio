47c1af363742341e4d2ff90119c9644149e89431
demaiodev <fml1041@gmail.com>
2025-10-03 13:14:46 -0400

    add more pet images because im pet crazy

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -6,13 +6,20 @@ import img1773 from "../../pics/IMG_1773.webp";
 import img2532 from "../../pics/IMG_2532.webp";
 import img3476 from "../../pics/IMG_3476.webp";
 import img4949 from "../../pics/IMG_4949.webp";
-import img5082 from "../../pics/IMG_5082_jpg.webp";
+import img5082 from "../../pics/IMG_5082.webp";
 import img5264 from "../../pics/IMG_5264.webp";
 import img5706 from "../../pics/IMG_5706.webp";
 import img5707 from "../../pics/IMG_5707.webp";
 import img5947 from "../../pics/IMG_5947.webp";
 import img6613 from "../../pics/IMG_6613.webp";
 import img8385 from "../../pics/IMG_8385.webp";
+import img0171 from "../../pics/IMG_0171.webp";
+import img0409 from "../../pics/IMG_0409.webp";
+import img0411 from "../../pics/IMG_0411.webp";
+import img1936 from "../../pics/IMG_1936.webp";
+import img2095 from "../../pics/IMG_2095.webp";
+import img6343 from "../../pics/IMG_6343.webp";
+import img9999 from "../../pics/IMG_9999.webp";
 
 const AboutSection: React.FC = () => {
   const { currentTheme } = useTheme();
@@ -30,6 +37,13 @@ const AboutSection: React.FC = () => {
     img5947,
     img6613,
     img8385,
+    img0171,
+    img0409,
+    img0411,
+    img1936,
+    img2095,
+    img6343,
+    img9999,
   ];
 
   return (
@@ -39,22 +53,20 @@ const AboutSection: React.FC = () => {
       </h2>
       <p className={`${currentTheme.text} mb-2`}>
         I'm 34 and I live in Sarasota, Florida. I like it here, except the
-        weather during the summer. Also hurricanes happen, a lot.
+        weather during the summer. And the hurricanes, they suck somtimes. Once
+        fall and winter are here, though, its the place to be.
       </p>
       <p className={`${currentTheme.text} mb-2`}>
-        When I'm not programming, I'm probably playing video games. My current
-        Steam game rotation is Tekken, Factorio, Deadlock and Age of Empires II.
-        I also enjoy playing and creating music - I like to pretend I can play
-        the guitar, and I also play the piano. I use Ableton or FL Studio to
-        record and produce songs.
+        When I'm not programming, I'm probably playing video games on Steam. I
+        enjoy playing and creating music - I like to pretend I can play the
+        guitar, and I can also play the piano. I use FL Studio to record and
+        produce songs.
       </p>
       <p className={`${currentTheme.text} mb-2`}>
         I have 2 four-legged creatures in my house. I have a Persian cat named
         Pancake (because her face is smushed) and an English Cream golden
         retriever named Charlie. Of course I have included pictures of them,
-        because I like to share. Charlie and I are avid tennis ball fetchers,
-        and Pancake is a milk-drinking machine. She needs a special bowl because
-        she is without a snout.
+        because I like to share.
       </p>
 
       <div className="w-full mt-6">
