a7099dc1e53b022bdcd7b2cdeeced964809340e9
demaiodev <fml1041@gmail.com>
2025-10-15 15:50:57 -0400

    link's should be containers here

--- src/components/LandingHero.tsx
+++ src/components/LandingHero.tsx
@@ -13,31 +13,31 @@ const LandingHero: React.FC<{ textColor?: string }> = ({ textColor }) => {
       </h1>
       <p className={`text-2xl mb-4`}>Full-Stack Engineer & Web Developer.</p>
       <div className="flex mb-4 justify-center items-center">
-        <div
-          className=" mx-1 p-3 rounded-full transition-all duration-300 hover:scale-110 focus:outline-none"
-          style={{
-            color: "var(--accent-100)",
-            backgroundColor: "var(--accent-600)",
-          }}
+        <Link
+          to="https://www.linkedin.com/in/christopher-demaio/"
+          target="_blank"
         >
-          <Link
-            to="https://www.linkedin.com/in/christopher-demaio/"
-            target="_blank"
+          <div
+            className=" mx-1 p-3 rounded-full transition-all duration-300 hover:scale-110 focus:outline-none"
+            style={{
+              color: "var(--accent-100)",
+              backgroundColor: "var(--accent-600)",
+            }}
           >
             <Linkedin size={35} />
-          </Link>
-        </div>
-        <div
-          className=" mx-1 p-3 rounded-full transition-all duration-300 hover:scale-110 focus:outline-none"
-          style={{
-            color: "var(--accent-100)",
-            backgroundColor: "var(--accent-600)",
-          }}
-        >
-          <Link to="https://github.com/demaiodev" target="_blank">
+          </div>
+        </Link>
+        <Link to="https://github.com/demaiodev" target="_blank">
+          <div
+            className=" mx-1 p-3 rounded-full transition-all duration-300 hover:scale-110 focus:outline-none"
+            style={{
+              color: "var(--accent-100)",
+              backgroundColor: "var(--accent-600)",
+            }}
+          >
             <Github size={35} />
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
       <p className={`text-md mb-16`}>
         I've been creating web applications and software using{" "}
